dad6da3ab1c651bd4418102f183cbb951fce478d
Yevgeniya-Lat <e9618281262@gmail.com>
2022-03-07 19:43:52 +0300

    new Task

---
+++ Task_01/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу которая будет рассчитывать площадь круга;
+double r,perimeter, area;
+Console.WriteLine("Введите радиус вашего круга  ");
+r = Convert.ToDouble(Console.ReadLine());
+perimeter = 2*3.14*r;
+area = 3.14 * Math.Pow(r,2);
+Console.WriteLine("Периметр вашего круга {0}" ,perimeter);
+Console.WriteLine("Площадь вашего круга: {0}",area);
+Console.ReadKey();
---
+++ Task_02/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] masiv = new int[5]{5,7,2,9,1};
+Console.WriteLine("Длина массива:  " + masiv.Length);
+Console.Write ("Массив до сортировки: ");
+for(int i = 0; i<masiv.Length; i++)
+    Console.Write(" " +masiv[i]);
+Array.Sort(masiv);
+Console.WriteLine();
+Console.Write("Отсортированный массив:  ");
+    for(int i=0; i<masiv.Length; i++)
+Console.Write(" " +masiv[i]);
+Console.ReadKey();
+{
+    
+}
